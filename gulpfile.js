91062d5d3eb9370a05a1b8c918ca9c08e4e5e581
ReeYoungJe <zofhd2@naver.com>
2023-01-27 17:37:30 +0900

    gulp 폴더구성 좀 바뀜

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,106 @@
-const {task, dest, src, watch, series} = require("gulp");
+const {task, dest, src, watch, series,} = require("gulp");
 const uglify = require('gulp-uglify')
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps')
 const concat = require('gulp-concat');
 const sass = require('gulp-sass')(require('sass'));
-const csso = require('gulp-csso')
-const fs = require('fs')
+const csso = require('gulp-csso');
+const fs = require('fs');
+const replace = require('gulp-replace');
+const insert = require('gulp-insert');
+const csscomb = require('gulp-csscomb');
+
 /**
  * ==============================+
  * 경로
  * ==============================+
  */
 
-const group = './src';
-const dist = './dist';
 const paths = {
-    html : './**/*.html',
-    scss : group + '/**/*.scss',
-    dist : group + '/dist/css/_entry/*.css',
-    js : group + '/js/**/*.js',
-};
-
-const pageArr = [
-    paths.dist,
-]
-
-/**
-* ==============================+
-* @SCSS : SCSS Config(환경설정)
-* ==============================+
-*/
-const scssOptions = {
-    /**
-     * CSS의 컴파일 결과 코드스타일 지정
-     * Values : nested, expanded, compact, compressed
-     *
-     * sourceComments (Type : Boolean , Default : false)
-     * 컴파일 된 CSS 에 원본소스의 위치와 줄수 주석표시.
-     */
-    outputStyle : "compressed",
-    sourceComments: true
-
+    scss: './src/common/scss/**/*.scss',
+    scssDist: './src/dist/css/_entry/**/*.css',
+    js: './src/common/js/*.js',
+    jsDist: './src/dist/js/**/*.js'
 };
 
 /**
- * 추가 해야될것
- *  csso
- *  csscomb
- *
+ * ==============================+
+ * @SCSS : SCSS Config(환경설정)
+ * ==============================+
  */
-
-const sassFn = (done) =>{
+const sassFn = (done) => {
 
     src(paths.scss)
-        .pipe(sourcemaps.init({loadMaps: true})) //소스 Map 생성
-        .pipe(sass({outputStyle: 'expanded',sourceComments:  true}).on('error', sass.logError)) //sass 컴파일
+        .pipe(sass.sync({outputStyle: 'expanded'}).on('error', sass.logError)) //sass 컴파일
+        //.pipe()
         .pipe(autoprefixer({cascade: true})) //vendor - prefix 달아주기
-        .pipe(csso())
+        //.pipe(csso())
+        .pipe(replace('"UTF-8";', '"UTF-8"\n'))
+        .pipe(replace('@charset "UTF-8";', ''))
+        .pipe(insert.prepend('@charset "UTF-8"; \n'))
+        .pipe(replace(/}/g, '}\n'))
+        //.pipe(replace('/*!', '\n/*!'))
+        .pipe(replace('{.', '{\n\t.'))
         .pipe(sourcemaps.write('./maps')) // 소스맵 뿌려주고
-        .pipe(dest(group + '/dist/css/')) //css 폴더에 styles 생성
-    src(paths.dist)
-        .pipe(concat('style.min.css'))
-        .pipe(dest(group + '/dist/css/')) //css 폴더에 styles 생성
+        .pipe(dest('./src/dist/css/')) //css 폴더에 styles 생성
+    src(paths.scssDist)
+        //.pipe(csso())
+        .pipe(replace('@charset "UTF-8";', ''))
+        .pipe(concat('common.min.css')) //./src/dist/css/_entry/**/*.css 에 모든 css 파일을  합치기
+        .pipe(dest('./src/dist/css/'))
+        .pipe(replace('"UTF-8"', '"UTF-8"; \n'))
+        .pipe(insert.prepend('@charset "UTF-8"; \n'))
+        .on('end', () => {
+            console.log('./src/dist/css/common.min.css DIst 배포 완료')
+        })
 
-    if(done){
+    if (done) {
         done();
     }
 }
+
+const jsFn = (done) => {
+    src(paths.js)
+        .pipe(uglify())
+        .pipe(dest('./src/dist/js')); // dist 폴더에 병합한 파일 생성
+    if (done) {
+        done();
+    }
+
+}
+const jsDistFn = (done) => {
+    src(paths.jsDist)
+        .pipe(concat('main.min.js')) // main.js로 파일이름을 짓고 병합
+        .pipe(dest('./src/dist/js')); // dist 폴더에 병합한 파일 생성
+    if (done) {
+        done();
+    }
+}
+
+
 task('sass', sassFn);
+task('js', jsFn);
+task('js:dist', jsDistFn);
 
 
-task('watch',series('sass',(done)=>{
-    watch(paths.scss).on('change',(file)=>{
+task('watch', series('sass', (done) => {
+    watch(paths.scss).on('change', (file) => {
         sassFn();
-        console.log(file+'변경감지')
-    }).on('ready',()=>{
+        console.log(file + '변경감지')
+    }).on('ready', () => {
         done();
     });
 }));
 
 
-
-task('event',async (done)=>{
+task('event', async (done) => {
     const selectFolder = require('./node_custormModules/selectFolder');
     const eventPageFilter = require('./node_custormModules/eventPageFilter')
 
-    const eventHtml = await selectFolder('./src/resource/event/inc');
+    const eventHtml = await selectFolder('./src/resource/html/event/inc');
     const eventFiles = eventPageFilter(eventHtml);
 
-    await fs.writeFileSync('./src/convention/worklist/eventlist.json',JSON.stringify(eventFiles))
+    await fs.writeFileSync('./src/convention/events/eventList.json', JSON.stringify(eventFiles))
     done();
 })
 
